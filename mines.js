e0786d35a4eae9460b23eee1da7d1cd3c05109f7
amelkov <amelkov.ea@gmail.com>
2016-04-04 17:52:33 +0300

    Logic of game almost finished.

--- mines.js
+++ mines.js
@@ -1,68 +1,136 @@
 ﻿var numberMines = 10;
-var fieldValues = []; //неудобно проверять соседние клетки, переделать под массив
-fieldValues.length = 64; 
+var columns = 8, rows = 8;
+var field = [];
+var countMoves = 0;
 
 var container = document.createElement("div");
 container.className = 'container';
 container.id = 'container';
 document.body.appendChild(container);
 
-var field = document.createElement("div");
-field.className = 'field';
-field.id = 'field';
-container.appendChild(field);
+var fieldView = document.createElement("div");
+fieldView.className = 'field';
+fieldView.id = 'field';
+container.appendChild(fieldView);
 
-for(i=1;i<=fieldValues.length;i++){
-	(function (i) {
-		var cell = document.createElement("div");
-		cell.className = 'cell';
-		cell.id = 'cell'+i;
-		field.appendChild(cell);
-		cell.addEventListener('click', function(){clickCell(i)});
-	}(i));
+for(i = 0; i < rows; i++){
+	for(j = 0; j < columns; j++){
+		(function (i,j) {
+			var cell = document.createElement("div");
+			cell.className = 'cell';
+			cell.id = 'cell'+i+j;
+			fieldView.appendChild(cell);
+			cell.addEventListener('click', function(){clickCell(i,j)});
+			cell.addEventListener('contextmenu', function(ev){ev.preventDefault(); rightClickCell(i,j);});
+		}(i,j));
+	}
 }
+
+var testCounter = document.createElement("div");
+testCounter.id = 'counter';
+testCounter.innerHTML = countMoves;
+container.appendChild(testCounter);
+
+createField();
 
 //x(-1) - мина или число
 function createField(){
+	countMoves = 0;
+	document.getElementById("counter").innerHTML = countMoves;
+	
+	for(i = 0; i < rows; i++){
+		field[i] = [];
+		for(j = 0; j < columns; j++){
+			field[i][j] = 0;
+			document.getElementById("cell"+i+j).innerHTML = "";
+		}
+	}
+
 	for(i = 0; i < numberMines; i++){
-		var j = randomCell();
-		if(fieldValues[j] == null){
-			fieldValues[j] = -1;
+		var xy = randomCell();
+		if(field[xy[0]][xy[1]] == 0){
+			field[xy[0]][xy[1]] = -1;
 		}else {i--;}
 	}
 	
-	for(i = 0; i<fieldValues.length; i++){
-		if(fieldValues[i] != -1){
-			
+	for(i = 0; i < rows; i++){
+		for(j = 0; j < columns; j++){
+			if(field[i][j] != -1){
+				field[i][j] = countMines(i,j);
+			}
 		}
 	}
 }
-//тоже переделать под матрицу
-function randomCell(){
-	return Math.floor(Math.random() * 64);
-}
-/*
-нужно будет переделать под матрицу
 
-пример создания
-function matrixArray(rows,columns){
-  var arr = new Array();
-  for(var i=0; i<columns; i++){
-    arr[i] = new Array();
-    for(var j=0; j<rows; j++){
-      arr[i][j] = i+j+1;
-    }
-  }
-  return arr;
+function randomCell(){
+	return [Math.floor(Math.random() * 8), Math.floor(Math.random() * 8)];
 }
-var myMatrix = matrixArray(3,3);
-*/
-function countMines(cell){
-	var count = 0;
-	if(cell % 8 && fieldValues[cell-9] == -1){count++;}
-	if(cell > 7 && fieldValues[cell-8] == -1){count++;}
-	if(cell > 6 && fieldValues[cell-7] == -1){count++;}
-	if(cell > 0 && fieldValues[cell-1] == -1){count++;}
-	if(cell > 8 && fieldValues[cell-8] == -1){count++;}
+
+function countMines(i,j){
+	if(field[i][j] == -1){return -1;}
 	
+	var count = 0;
+	
+	if(i > 0 && j > 0 && field[i-1][j-1] == -1){count++;}
+	if(i > 0 && field[i-1][j] == -1){count++;}
+	if(i > 0 && j < columns-1 && field[i-1][j+1] == -1){count++;}
+	
+	if(j > 0 && field[i][j-1] == -1){count++;}
+	if(j < columns-1 && field[i][j+1] == -1){count++;}
+	
+	if(i < rows-1 && j > 0 && field[i+1][j-1] == -1){count++;}
+	if(i < rows-1 && field[i+1][j] == -1){count++;}
+	if(i < rows-1 && j < columns-1 && field[i+1][j+1] == -1){count++;}
+	
+	return count;
+}
+
+function clickCell(i,j){
+	if(field[i][j] == -1){
+		defeat();
+	}else if(document.getElementById("cell"+i+j).innerHTML == ''){
+		document.getElementById("cell"+i+j).innerHTML = field[i][j];
+		countMoves++;
+		document.getElementById("counter").innerHTML = countMoves;
+		if(field[i][j] == 0){chain(i,j);}
+		if(countMoves == columns*rows-numberMines){win();}
+	}
+}
+
+function chain(i,j){
+	if(i > 0 && j > 0 && document.getElementById("cell"+(i-1)+(j-1)).innerHTML == ''){clickCell(i-1,j-1);}
+	if(i > 0 && document.getElementById("cell"+(i-1)+j).innerHTML == ''){clickCell(i-1,j);}
+	if(i > 0 && j < columns-1 && document.getElementById("cell"+(i-1)+(j+1)).innerHTML == ''){clickCell(i-1,j+1);}
+	
+	if(j > 0 && document.getElementById("cell"+i+(j-1)).innerHTML == ''){clickCell(i,j-1);}
+	if(j < columns-1 && document.getElementById("cell"+i+(j+1)).innerHTML == ''){clickCell(i,j+1);}
+	
+	if(i < rows-1 && j > 0 && document.getElementById("cell"+(i+1)+(j-1)).innerHTML == ''){clickCell(i+1,j-1);}
+	if(i < rows-1 && document.getElementById("cell"+(i+1)+j).innerHTML == ''){clickCell(i+1,j);}
+	if(i < rows-1 && j < columns-1 && document.getElementById("cell"+(i+1)+(j+1)).innerHTML == ''){clickCell(i+1,j+1);}
+}
+
+function rightClickCell(i,j){
+	if(document.getElementById("cell"+i+j).innerHTML == ''){
+		document.getElementById("cell"+i+j).innerHTML = "p";
+	}else if(document.getElementById("cell"+i+j).innerHTML == "p"){
+		document.getElementById("cell"+i+j).innerHTML = "";
+	}
+}
+
+function defeat(){
+	for(i = 0; i < rows; i++){
+		for(j = 0; j < columns; j++){
+			if(field[i][j] == -1){
+				document.getElementById("cell"+i+j).innerHTML = "*";
+			}
+		}
+	}
+	alert("u lose");
+	createField();
+}
+
+function win(){
+	alert("u win");
+	createField();
 }
